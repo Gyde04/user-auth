af51da6370ff3a9ada18731ea7d7e895f9aeb22e
Gyde04 <fakileemmanuel@gmail.com>
2024-01-02 07:55:04 +0100

    improvement to endpoints

--- server/views.py
+++ server/views.py
@@ -1,46 +1,49 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
-
-from .serializers import UserSerializer
 from rest_framework import status
-from rest_framework.authtoken.models import Token
-from django.contrib.auth.models import User
-
-from django.shortcuts import get_object_or_404
-
-# login endpoint
-@api_view(['POST'])
-def login(request):
-    user =  get_object_or_404(User, username=request.data['username'])
-    if not user.check_password(request.data['password']):
-        return Response({"detail": "Not found."}, status=status.HTTP_400_BAD_REQUEST)
-    token, created = Token.objects.get_or_create(user=user)
-    serializer = UserSerializer(instance=user)
-    return Response({"token": token.key, "user": serializer.data})
-
-# sign up endpoint
-@api_view(['POST'])
-def signup(request):
-    serializer = UserSerializer(data=request.data)
-    if serializer.is_valid():
-        user = User(username=request.data.get('username'))
-        user.set_password(request.data.get('password'))  # Set password before saving
-        user.save()
-
-        # Create token for the user
-        token = Token.objects.create(user=user)
-
-        # Return token and user data in the response
-        return Response({"token": token.key, "user": serializer.data})
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from rest_framework.authtoken.models import Token
 
-# Test token end
+from .serializers import UserSerializer
+
+@api_view(['POST'])
+def login(request):
+    """
+    Log in a user and return a token.
+    """
+    try:
+        user = get_object_or_404(User, username=request.data.get('username'))
+        if not user.check_password(request.data.get('password')):
+            return Response({"detail": "Invalid credentials."}, status=status.HTTP_400_BAD_REQUEST)
+        token, created = Token.objects.get_or_create(user=user)
+        serializer = UserSerializer(instance=user)
+        return Response({"token": token.key, "user": serializer.data})
+    except Exception as e:
+        return Response({"detail": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+@api_view(['POST'])
+def signup(request):
+    """
+    Sign up a new user.
+    """
+    try:
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = User.objects.create_user(username=request.data.get('username'), password=request.data.get('password'))
+            token = Token.objects.create(user=user)
+            return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except Exception as e:
+        return Response({"detail": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 @api_view(['GET'])
 @authentication_classes([SessionAuthentication, TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def test_token(request):
-    return Response("passed for {}".format(request.user.email))
+    """
+    Test authentication token.
+    """
+    return Response("Authentication passed for {}".format(request.user.username))
