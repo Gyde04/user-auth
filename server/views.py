6930bde17f6cc28347a04f175a650f453a5833dd
Gyde04 <fakileemmanuel@gmail.com>
2024-03-13 11:34:23 +0100

    feat: signup done

--- server/views.py
+++ server/views.py
@@ -1,13 +1,30 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .serializers import UserSerializer
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+
 @api_view(['POST'])
 def login(request):
     return Response({})
 
 @api_view(['POST'])
 def signup(request):
-    return Response({})
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        user = User(username=request.data.get('username'))
+        user.set_password(request.data.get('password'))  # Set password before saving
+        user.save()
+
+        # Create token for the user
+        token = Token.objects.create(user=user)
+
+        # Return token and user data in the response
+        return Response({"token": token.key, "user": serializer.data})
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['GET'])
 def test_token(request):
